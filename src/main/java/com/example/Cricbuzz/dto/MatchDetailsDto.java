5b9bf08905110a3a8f0be9427e7537eec2732a15
Gunjan <glunkad007@gmail.com>
2024-05-31 22:19:53 +0530

    feat: added MatchDetailsDto

---
+++ src/main/java/com/example/Cricbuzz/dto/MatchDetailsDto.java
@@ -0,0 +1,34 @@
+package com.example.Cricbuzz.dto;
+
+import com.example.Cricbuzz.model.Team;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MatchDetailsDto {
+    private long id;
+
+    private String venue;
+
+    private LocalDate date;
+
+    private String team1;
+
+    private String team2;
+
+//    private List<Team> squads;
+    private List<TeamDto> squads;
+
+}
+
+
+
+
